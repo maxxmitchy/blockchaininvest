0ac554fdc66f843bc24ad92804cd29a01b8034bc
MAxMitCHy <mitchycarl6@gmail.com>
2020-11-02 10:45:07 +0100

    refactored Deposit.js

--- resources/js/components/User/partials/Deposit.js
+++ resources/js/components/User/partials/Deposit.js
@@ -1,60 +1,136 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { BsArrowLeft } from "react-icons/bs";
-import { FaInfoCircle } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { FaAngleDown, FaInfoCircle } from "react-icons/fa";
+import { useQuery } from "react-query";
 
 const Deposit = () => {
-    const [condition, setCondition] = useState("Copy and process request");
-
-    let inputRef = useRef(null);
-
-    const handleClick = () => {
-        inputRef.current.select();
-        condition !== "Address copied" && myInp.select();
-        document.execCommand("Copy");
-        setCondition("Address Copied");
+    const getWallets = async () => {
+        const data = await axios.get("api/wallets");
+        return data;
     };
 
-    const handleChange = e => {};
+    const { data } = useQuery("wallets", getWallets);
+
+    const [deposit, setDeposit] = useState({
+        open: false,
+        currency: "",
+        address: "",
+        Image: "",
+        amount: ""
+    });
+
+    useEffect(() => {
+        setDeposit({
+            ...deposit,
+            currency: data?.data[0].name,
+            Image: data?.data[0].image,
+            address: data?.data[0].address
+        });
+    }, [data]);
+
+    const handleChange = e => {
+        let value = e.target.value;
+        setDeposit({
+            [e.target.name]: value
+        });
+    };
 
     return (
         <React.Fragment>
-            <h3 className="text-center mt-md-4 mb-md-5 font-weight-bolder">
+            <h3 className="text-center my-4 mb-md-5 font-weight-bolder">
                 Make Deposit
             </h3>
+
             <div className="row">
-                <div className="col-md-6 ">
-                    <div className="d-flex flex-column justify-content-center align-items-center">
-                        <img className="img-fluid mt-3" src="img/scan.png" />
+                <div className="col-md-6">
+                    <div
+                        onClick={() => {
+                            setDeposit({ ...deposit, open: !deposit.open });
+                        }}
+                        className="d-flex justify-content-between border-bottom"
+                    >
+                        <div className="d-flex">
+                            <img
+                                src={`${deposit.Image}`}
+                                className="img-fluid mr-2"
+                                style={{
+                                    width: "30px",
+                                    height: "20px"
+                                }}
+                                alt=""
+                            />
+                            <b>{deposit.currency}</b>
+                        </div>
+                        <FaAngleDown />
                     </div>
+                    {deposit.open && (
+                        <div className="border rounded mt-2 d-flex flex-column px-1 py-3 ">
+                            {data?.data.map(i => {
+                                return (
+                                    <div
+                                        onClick={() => {
+                                            setDeposit({
+                                                ...deposit,
+                                                open: !deposit.open,
+                                                currency: i.name,
+                                                Image: i.image
+                                            });
+                                        }}
+                                        className="d-flex my-2 currency_select py-2 rounded"
+                                        key={i.id}
+                                    >
+                                        <img
+                                            src={`${i.image}`}
+                                            className="img-fluid mr-2"
+                                            style={{
+                                                width: "30px",
+                                                height: "20px"
+                                            }}
+                                            alt=""
+                                        />
+                                        <b>{i.name}</b>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    )}
                     <input
-                        type="text"
-                        value="1P5mw1q4RNNWxEx1a8MfbcUvXMydxva35H"
-                        className="bg-white w-100 my-2"
-                        style={{ border: "none", outline: "none" }}
+                        className={`mt-3 w-100 `}
+                        style={{
+                            outline: "none",
+                            border: "1px solid white",
+                            borderBottom: "1px solid lightgrey"
+                        }}
+                        type="number"
+                        name="amount"
                         onChange={handleChange}
-                        id="myInp"
-                        ref={inputRef}
+                        value={deposit.amount}
+                        placeholder="please enter deposit amount e.g., 1000"
                     />
+                    <div className="d-flex justify-content-center align-items-center">
+                        <img
+                            className="img-fluid mt-3  shadow rounded p-2 bg-white mb-3"
+                            src="img/scan.png"
+                        />
+                    </div>
                     <div className="d-flex justify-content-center align-items-center">
                         <button
                             id="myBut"
-                            onClick={handleClick}
+                            // onClick={handleClick}
                             className="btn-block deposit_button text-white"
                         >
-                            {condition}
+                            Process Request
                         </button>
                     </div>
-                    <br />
                 </div>
                 <div className="col-md-6 mt-5 mt-md-1">
                     <div className="d-flex text-danger ml-md-4">
                         <FaInfoCircle className="mr-1 mt-1" />
-                        <h4>Important !!!</h4>
+                        <h5>Important !!!</h5>
                     </div>
                     <ul>
                         <li>
-                            Send only BTC to this deposit address. Sending other
+                            Send only {deposit.currency} to this deposit address. Sending other
                             coins may result in the loss of your crypto asset.
                         </li>
                         <li>
